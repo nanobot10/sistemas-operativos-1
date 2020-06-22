074a305d3034fbdf196ded66ea317633f276ecc1
dennis.castro <c.alejandro17@gmail.com>
2020-06-21 23:02:14 -0600

    ubuntu python nats subscriber

---
+++ proyecto2/python-nats-sub/__main__.py
@@ -0,0 +1,59 @@
+import asyncio
+import os
+import signal
+import json
+from nats.aio.client import Client as NATS
+
+
+async def run(loop):
+    nc = NATS()
+
+    async def closed_cb():
+        print("Connection to NATS is closed.")
+        await asyncio.sleep(0.1, loop=loop)
+        loop.stop()
+
+    # It is very likely that the demo server will see traffic from clients other than yours.
+    # To avoid this, start your own locally and modify the example to use it.
+    options = {
+        "servers": ["nats://10.20.1.2:4222"],
+        # "servers": ["nats://0.0.0.0:4222"],
+        "loop": loop,
+        "closed_cb": closed_cb
+    }
+
+    await nc.connect(**options)
+    print(f"Connected to NATS at {nc.connected_url.netloc}...")
+
+    async def subscribe_handler(msg):
+        subject = msg.subject
+        reply = msg.reply
+        data = json.loads(msg.data.decode())
+        print("Received a message on '{subject} {reply}': {data}".format(
+            subject=subject, reply=reply, data=data))
+
+    # Basic subscription to receive all published messages
+    # which are being sent to a single topic 'discover'
+    await nc.subscribe("foo", cb=subscribe_handler)
+
+    # Subscription on queue named 'workers' so that
+    # one subscriber handles message a request at a time.
+    await nc.subscribe("foo.*", "workers", subscribe_handler)
+
+    def signal_handler():
+        if nc.is_closed:
+            return
+        print("Disconnecting...")
+        loop.create_task(nc.close())
+
+    for sig in ('SIGINT', 'SIGTERM'):
+        loop.add_signal_handler(getattr(signal, sig), signal_handler)
+
+
+if __name__ == '__main__':
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(run(loop))
+    try:
+        loop.run_forever()
+    finally:
+        loop.close()
