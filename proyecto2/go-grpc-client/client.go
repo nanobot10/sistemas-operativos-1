2b729fe3f254c2dc14808dabf12b110a9245adba
dennis.castro <c.alejandro17@gmail.com>
2020-06-27 21:52:50 -0600

    python save db mongo and redis

--- proyecto2/go-grpc-client/client.go
+++ proyecto2/go-grpc-client/client.go
@@ -1,14 +1,46 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
 	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
 
 	pb "github.com/nanobot10/sistemas-operativos-1/proyecto2/go-grpc-client/calculator"
 	"golang.org/x/net/context"
 	"google.golang.org/grpc"
 )
 
-func main() {
+/*Paciente objeto del archivo de entrada*/
+type Paciente struct {
+	Nombre        string `json:"nombre"`
+	Departamento  string `json:"departamento"`
+	Edad          int    `json:"edad"`
+	FormaContagio string `json:"Forma de contagio"`
+	Estado        string `json:"estado"`
+}
+
+type Response struct {
+	status  int    `json:"status"`
+	message string `json:"message"`
+}
+
+func handleRequests() {
+	// creates a new instance of a mux router
+	myRouter := mux.NewRouter().StrictSlash(true)
+	myRouter.HandleFunc("/", sendPaciente).Methods("POST")
+	log.Fatal(http.ListenAndServe(":5000", myRouter))
+}
+
+func sendPaciente(w http.ResponseWriter, r *http.Request) {
+
+	reqBody, _ := ioutil.ReadAll(r.Body)
+	var paciente Paciente
+	json.Unmarshal(reqBody, &paciente)
+
 	var conn *grpc.ClientConn
 	conn, err := grpc.Dial("localhost:50051", grpc.WithInsecure())
 	if err != nil {
@@ -20,7 +52,9 @@ func main() {
 	client := pb.NewCalculatorClient(conn)
 
 	message := pb.Number{
-		Value: 16,
+		nombre:       paciente.Nombre,
+		departamento: paciente.Departamento,
+		edad:         paciente.Edad,
 	}
 
 	response, err := client.SquareRoot(context.Background(), &message)
@@ -30,5 +64,9 @@ func main() {
 	}
 
 	log.Printf("response from server: %f", response.Value)
+}
 
+func main() {
+	fmt.Println("init go rest api")
+	handleRequests()
 }
